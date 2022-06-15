e41dc757a620049a537f0932631e9b9a7f2ae2d9
Atul kanekar <kanekar.atul@gmail.com>
2022-06-16 01:49:34 +0530

    update-1

--- index.js
+++ index.js
@@ -6,6 +6,16 @@
  * MIT Licensed
  */
 
-'use strict';
+// 'use strict';
 
-module.exports = require('./lib/express');
+// module.exports = require('./lib/express');
+
+
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(3000)
